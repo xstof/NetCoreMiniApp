47fa8046d0cf11dd637b9545c1509a145756c1eb
Christof <no.email@noemail.com>
2018-12-04 21:02:49 +0100

    add docker image for app service with ssh support

---
+++ DockerForAzureAppService/init_container.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+cat >/etc/motd <<EOL 
+  _____                               
+  /  _  \ __________ _________   ____  
+ /  /_\  \\___   /  |  \_  __ \_/ __ \ 
+/    |    \/    /|  |  /|  | \/\  ___/ 
+\____|__  /_____ \____/ |__|    \___  >
+        \/      \/                  \/ 
+A P P   S E R V I C E   O N   L I N U X
+
+Documentation: http://aka.ms/webapp-linux
+
+EOL
+cat /etc/motd
+
+service ssh start
+
+
+STARTUPCOMMAND="dotnet /home/site/wwwroot/MyMvc.dll"
+echo "Running $STARTUPCOMMAND"
+eval "exec $STARTUPCOMMAND"
